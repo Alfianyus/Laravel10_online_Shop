9a2dbdc5a764ec9f977972106c78cf9b0c32e511
alfian.yuska <alfian.yuska@phiraka.com>
2023-06-20 10:38:37 +0700

    UpdateListSubCategory

--- resources/views/admin/sub_category/create.blade.php
+++ resources/views/admin/sub_category/create.blade.php
@@ -19,51 +19,57 @@
 <section class="content">
     <!-- Default box -->
     <div class="container-fluid">
-        <form action="" name="subCategoryForm" id="subCategoryForm"></form>
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="mb-3">
-                            <label for="name">Category</label>
-                            <select name="category" id="category" class="form-control">
-                                @if($categories->isNotEmpty())
-                                @foreach($categories as $category)
-                                <option value="{{$category->id}}">{{$category->name}}</option>
-                                @endforeach
-                                @endif
-                                <option value="">Mobile</option>
-                            </select>
+        <form action="" name="subCategoryForm" id="subCategoryForm">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="mb-3">
+                                <label for="name">Category</label>
+                                <select name="category" id="category" class="form-control">
+                                    <option value="">Select a category</option>
+                                    @if($categories->isNotEmpty())
+                                    @foreach($categories as $category)
+                                    <option value="{{$category->id}}">{{$category->name}}</option>
+                                    @endforeach
+                                    @endif
+                                    <option value="">Mobile</option>
+                                </select>
+                                <p></p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="mb-3">
-                            <label for="name">Name</label>
-                            <input type="text" name="name" id="name" class="form-control" placeholder="Name">
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <label for="name">Name</label>
+                                <input type="text" name="name" id="name" class="form-control" placeholder="Name">
+                                <p></p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="mb-3">
-                            <label for="slug">Slug</label>
-                            <input type="text" readonly name="slug" id="slug" class="form-control" placeholder="Slug">
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <label for="slug">Slug</label>
+                                <input type="text" readonly name="slug" id="slug" class="form-control" placeholder="Slug">
+                                <p></p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="mb-3">
-                            <label for="status">Status</label>
-                            <select name="status" id="status" class="form-control">
-                                <option value="1">Active</option>
-                                <option value="0">Block</option>
-                            </select>
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <label for="status">Status</label>
+                                <select name="status" id="status" class="form-control">
+                                    <option value="1">Active</option>
+                                    <option value="0">Block</option>
+                                </select>
+                                <p></p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="pb-5 pt-3">
-            <button class="btn btn-primary">Create</button>
-            <a href="subcategory.html" class="btn btn-outline-dark ml-3">Cancel</a>
-        </div>
+            <div class="pb-5 pt-3">
+                <button type="submit" class="btn btn-primary">Create</button>
+                <a href="subcategory.html" class="btn btn-outline-dark ml-3">Cancel</a>
+            </div>
+        </form>
     </div>
     <!-- /.card -->
 </section>
@@ -86,17 +92,17 @@
 
                 if (response["status"] == true) {
 
-                    window.location.href = "{{route('categories.index')}}";
+                    // window.location.href = "{{route('categories.index')}}";
 
-                    $("#name").removeClass('is-invalid')
-                        .siblings('p')
-                        .removeClass('invalid-feedback')
-                        .html("");
+                    // $("#name").removeClass('is-invalid')
+                    //     .siblings('p')
+                    //     .removeClass('invalid-feedback')
+                    //     .html("");
 
-                    $("#slug").removeClass('is-invalid')
-                        .siblings('p')
-                        .removeClass('invalid-feedback')
-                        .html("");
+                    // $("#slug").removeClass('is-invalid')
+                    //     .siblings('p')
+                    //     .removeClass('invalid-feedback')
+                    //     .html("");
 
                 } else {
                     var errors = response['errors'];
@@ -125,6 +131,21 @@
                             .html("");
 
                     }
+
+                    if (errors['category']) {
+                        $("#category").addClass('is-invalid')
+                            .siblings('p')
+                            .addClass('invalid-feedback')
+                            .html(errors['category']);
+                    } else {
+                        $("#category").removeClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback')
+                            .html("");
+
+                    }
+
+
                 }
 
             },
