f4d0d65250bd17dc71ad245547d610ba9e41955f
alfian.yuska <alfian.yuska@phiraka.com>
2023-06-20 15:13:04 +0700

    AddBrandCategory

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\admin\AdminLoginController;
+use App\Http\Controllers\admin\BrandController;
 use App\Http\Controllers\admin\CategoryController;
 use App\Http\Controllers\admin\HomeController;
 use App\Http\Controllers\admin\SubCategoryController;
@@ -40,30 +41,25 @@ Route::group(['prefix' => 'admin'], function () {
 
         //Category Routes
         Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
-
         Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
         Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
-
         Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
-
         Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
-
         Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.delete');
 
-        //Sub Category Route
+        //Sub Category Routes
         Route::get('/sub-categories', [SubCategoryController::class, 'index'])->name('sub-categories.index');
-
-
         Route::get('/sub-categories/create', [SubCategoryController::class, 'create'])->name('sub-categories.create');
-
         Route::post('/sub-categories', [SubCategoryController::class, 'store'])->name('sub-categories.store');
-
         Route::get('/sub-categories/{subCategory}/edit', [SubCategoryController::class, 'edit'])->name('sub-categories.edit');
-
         Route::put('/sub-categories/{subCategory}', [SubCategoryController::class, 'update'])->name('sub-categories.update');
-
-
         Route::delete('/sub-categories/{subCategory}', [SubCategoryController::class, 'destroy'])->name('sub-categories.delete');
+
+        //Brands Routes
+        Route::get('/brands/create', [BrandController::class, 'create'])->name('brands.create');
+        Route::post('/brands', [BrandController::class, 'store'])->name('brands.store');
+
+
 
 
 
